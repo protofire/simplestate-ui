4a5530d2ef58a0a9baa8589720f28b90983e8290
Agustín Longoni <agustin.longoni@altoros.com>
2022-11-14 14:17:17 -0300

    add tabs

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState } from "react";
 import {
   AppShell,
   Navbar,
@@ -10,7 +10,10 @@ import {
   Burger,
   useMantineTheme,
   Title,
-} from '@mantine/core';
+  Tabs,
+  Anchor,
+} from "@mantine/core";
+// import { IconPhoto, IconMessageCircle, IconSettings } from '@tabler/icons';
 
 export default function AppShellDemo() {
   const theme = useMantineTheme();
@@ -19,18 +22,26 @@ export default function AppShellDemo() {
     <AppShell
       styles={{
         main: {
-          background: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0],
+          background:
+            theme.colorScheme === "dark"
+              ? theme.colors.dark[8]
+              : theme.colors.gray[0],
         },
       }}
       navbarOffsetBreakpoint="sm"
       asideOffsetBreakpoint="sm"
       navbar={
-        <Navbar p="md" hiddenBreakpoint="sm" hidden={!opened} width={{ sm: 200, lg: 300 }}>
+        <Navbar
+          p="md"
+          hiddenBreakpoint="sm"
+          hidden={!opened}
+          width={{ sm: 200, lg: 300 }}
+        >
           <Text>Application navbar</Text>
         </Navbar>
       }
       aside={
-        <MediaQuery smallerThan="sm" styles={{ display: 'none' }}>
+        <MediaQuery smallerThan="sm" styles={{ display: "none" }}>
           <Aside p="md" hiddenBreakpoint="sm" width={{ sm: 200, lg: 300 }}>
             <Text>Application sidebar</Text>
           </Aside>
@@ -38,13 +49,17 @@ export default function AppShellDemo() {
       }
       footer={
         <Footer height={60} p="md">
-          Application footer
+          <Anchor href="https://mantine.dev/" target="_blank">
+            Simplestate docs
+          </Anchor>
         </Footer>
       }
       header={
         <Header height={{ base: 50, md: 70 }} p="md">
-          <div style={{ display: 'flex', alignItems: 'center', height: '100%' }}>
-            <MediaQuery largerThan="sm" styles={{ display: 'none' }}>
+          <div
+            style={{ display: "flex", alignItems: "center", height: "100%" }}
+          >
+            <MediaQuery largerThan="sm" styles={{ display: "none" }}>
               <Burger
                 opened={opened}
                 onClick={() => setOpened((o) => !o)}
@@ -59,6 +74,30 @@ export default function AppShellDemo() {
       }
     >
       <Title>Projects</Title>
+      <Tabs color="teal" variant="pills" radius="lg" defaultValue="invests" mt="lg">
+        <Tabs.List>
+          <Tabs.Tab value="invests">My Investments</Tabs.Tab>
+          <Tabs.Tab value="explorer">Projects Explorer</Tabs.Tab>
+          <Tabs.Tab value="admin">Admin Projects</Tabs.Tab>
+          <Tabs.Tab value="tressury">Tressury</Tabs.Tab>
+        </Tabs.List>
+
+        <Tabs.Panel value="invests" pt="xs">
+          My Investments tab content
+        </Tabs.Panel>
+
+        <Tabs.Panel value="explorer" pt="xs">
+          Projects Explorer tab content
+        </Tabs.Panel>
+
+        <Tabs.Panel value="admin" pt="xs">
+          Settings tab content
+        </Tabs.Panel>
+
+        <Tabs.Panel value="tressury" pt="xs">
+          Tressury tab content
+        </Tabs.Panel>
+      </Tabs>
     </AppShell>
   );
 }
