8ea2877c08b3fd5dcfafcf01c5087b973f5b3f7e
Agustín Longoni <agustin.longoni@altoros.com>
2022-11-14 14:02:34 -0300

    add basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,64 @@
-import { Container, MantineProvider, Text } from "@mantine/core"
+import { useState } from 'react';
+import {
+  AppShell,
+  Navbar,
+  Header,
+  Footer,
+  Aside,
+  Text,
+  MediaQuery,
+  Burger,
+  useMantineTheme,
+  Title,
+} from '@mantine/core';
 
-function App() {
+export default function AppShellDemo() {
+  const theme = useMantineTheme();
+  const [opened, setOpened] = useState(false);
   return (
-    <MantineProvider withGlobalStyles withNormalizeCSS>
-      <Container>
-        <Text>Simplestate</Text>
-      </Container>
-    </MantineProvider>
-  )
+    <AppShell
+      styles={{
+        main: {
+          background: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0],
+        },
+      }}
+      navbarOffsetBreakpoint="sm"
+      asideOffsetBreakpoint="sm"
+      navbar={
+        <Navbar p="md" hiddenBreakpoint="sm" hidden={!opened} width={{ sm: 200, lg: 300 }}>
+          <Text>Application navbar</Text>
+        </Navbar>
+      }
+      aside={
+        <MediaQuery smallerThan="sm" styles={{ display: 'none' }}>
+          <Aside p="md" hiddenBreakpoint="sm" width={{ sm: 200, lg: 300 }}>
+            <Text>Application sidebar</Text>
+          </Aside>
+        </MediaQuery>
+      }
+      footer={
+        <Footer height={60} p="md">
+          Application footer
+        </Footer>
+      }
+      header={
+        <Header height={{ base: 50, md: 70 }} p="md">
+          <div style={{ display: 'flex', alignItems: 'center', height: '100%' }}>
+            <MediaQuery largerThan="sm" styles={{ display: 'none' }}>
+              <Burger
+                opened={opened}
+                onClick={() => setOpened((o) => !o)}
+                size="sm"
+                color={theme.colors.gray[6]}
+                mr="xl"
+              />
+            </MediaQuery>
+            <img src="./src/assets/simple-state-logo.png" width={180} />
+          </div>
+        </Header>
+      }
+    >
+      <Title>Projects</Title>
+    </AppShell>
+  );
 }
-
-export default App
