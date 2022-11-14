2b5998fdba77a91f1a5f3f8723d44d7d0783515a
Agustín Longoni <agustin.longoni@altoros.com>
2022-11-14 16:21:04 -0300

    rm aside, navbar, footer

--- src/App.tsx
+++ src/App.tsx
@@ -3,57 +3,19 @@ import {
   AppShell,
   Navbar,
   Header,
-  Footer,
-  Aside,
   Text,
   MediaQuery,
   Burger,
   useMantineTheme,
   Title,
   Tabs,
-  Anchor,
 } from "@mantine/core";
-// import { IconPhoto, IconMessageCircle, IconSettings } from '@tabler/icons';
 
 export default function AppShellDemo() {
   const theme = useMantineTheme();
   const [opened, setOpened] = useState(false);
   return (
     <AppShell
-      styles={{
-        main: {
-          background:
-            theme.colorScheme === "dark"
-              ? theme.colors.dark[8]
-              : theme.colors.gray[0],
-        },
-      }}
-      navbarOffsetBreakpoint="sm"
-      asideOffsetBreakpoint="sm"
-      navbar={
-        <Navbar
-          p="md"
-          hiddenBreakpoint="sm"
-          hidden={!opened}
-          width={{ sm: 200, lg: 300 }}
-        >
-          <Text>Application navbar</Text>
-        </Navbar>
-      }
-      aside={
-        <MediaQuery smallerThan="sm" styles={{ display: "none" }}>
-          <Aside p="md" hiddenBreakpoint="sm" width={{ sm: 200, lg: 300 }}>
-            <Text>Application sidebar</Text>
-          </Aside>
-        </MediaQuery>
-      }
-      footer={
-        <Footer height={60} p="md">
-          <Anchor href="https://mantine.dev/" target="_blank">
-            Simplestate docs
-          </Anchor>
-        </Footer>
-      }
       header={
         <Header height={{ base: 50, md: 70 }} p="md">
           <div
