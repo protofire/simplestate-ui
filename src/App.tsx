eb0024019cbf1339a701b1dd31a2c8f25d506f0e
Marcos Carlomagno <marcoscarlomagno1@gmail.com>
2022-11-15 16:43:06 -0300

    lang: ui translation to spanish

--- src/App.tsx
+++ src/App.tsx
@@ -13,29 +13,29 @@ export default function App() {
         </Header>
       }
     >
-      <Title>Projects</Title>
+      <Title>Proyectos</Title>
       <Tabs color="teal" variant="pills" radius="lg" defaultValue="invests" mt="lg">
         <Tabs.List>
-          <Tabs.Tab value="invests">My Investments</Tabs.Tab>
-          <Tabs.Tab value="explorer">Projects Explorer</Tabs.Tab>
-          <Tabs.Tab value="admin">Admin Projects</Tabs.Tab>
-          <Tabs.Tab value="tressury">Tressury</Tabs.Tab>
+          <Tabs.Tab value="invests">Mis inversiones</Tabs.Tab>
+          <Tabs.Tab value="explorer">Proyectos</Tabs.Tab>
+          <Tabs.Tab value="admin">Administrar Proyectos</Tabs.Tab>
+          <Tabs.Tab value="tressury">Tesoro</Tabs.Tab>
         </Tabs.List>
 
         <Tabs.Panel value="invests" pt="xs">
-          My Investments tab content
+          Mis inversiones
         </Tabs.Panel>
 
         <Tabs.Panel value="explorer" pt="xs">
-          Projects Explorer tab content
+          Proyectos
         </Tabs.Panel>
 
         <Tabs.Panel value="admin" pt="xs">
-          Settings tab content
+          Administrar Proyectos
         </Tabs.Panel>
 
         <Tabs.Panel value="tressury" pt="xs">
-          Tressury tab content
+          Tesoro
         </Tabs.Panel>
       </Tabs>
     </AppShell>
