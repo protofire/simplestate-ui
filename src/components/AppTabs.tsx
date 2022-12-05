c45faf8faa629a1e388c3820301a6bc21b7d0e12
Agustín Longoni <agustin.longoni@altoros.com>
2022-12-05 12:27:42 -0300

    change tabs order

--- src/components/AppTabs.tsx
+++ src/components/AppTabs.tsx
@@ -2,7 +2,6 @@ import { Container, Tabs, Title } from "@mantine/core";
 import { useEffect, useState } from "react";
 import { Outlet, useLocation, useNavigate, useParams } from "react-router-dom";
 
-
 export function AppTabs() {
   const navigate = useNavigate();
   const { tabValue } = useParams();
@@ -10,34 +9,34 @@ export function AppTabs() {
   const location = useLocation();
 
   useEffect(() => {
-    if(!tabValue) {
-      setTab(location.pathname.slice(1))
+    if (!tabValue) {
+      setTab(location.pathname.slice(1));
     }
   }, []);
 
   const onTabChanged = (value: string) => {
     navigate(`/${value}`);
-    setTab(value)
+    setTab(value);
   };
 
   return (
     <Container>
-    <Tabs
-      value={tab}
-      onTabChange={onTabChanged}
-      color="teal"
-      variant="pills" 
-      radius="lg" 
-      mt="lg"
-    >
-      <Tabs.List>
-        <Tabs.Tab value="investments">Mis inversiones</Tabs.Tab>
-        <Tabs.Tab value="projects">Proyectos</Tabs.Tab>
-        <Tabs.Tab value="admin">Administrar Proyectos</Tabs.Tab>
-        <Tabs.Tab value="treasury">Tesoro</Tabs.Tab>
-      </Tabs.List>
-      <Outlet/>
-    </Tabs>
-  </Container>
-  )
+      <Tabs
+        value={tab}
+        onTabChange={onTabChanged}
+        color="teal"
+        variant="pills"
+        radius="lg"
+        mt="lg"
+      >
+        <Tabs.List>
+          <Tabs.Tab value="projects">Proyectos</Tabs.Tab>
+          <Tabs.Tab value="investments">Mis inversiones</Tabs.Tab>
+          <Tabs.Tab value="admin">Administrar Proyectos</Tabs.Tab>
+          <Tabs.Tab value="treasury">Tesoro</Tabs.Tab>
+        </Tabs.List>
+        <Outlet />
+      </Tabs>
+    </Container>
+  );
 }
