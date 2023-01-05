a6ff9350a43c7a608b87e405363dbaac79b01fa3
Marcos Carlomagno <marcoscarlomagno1@gmail.com>
2023-01-05 17:32:02 -0300

    feat: error notifications for withdraw

--- src/constants/errors.ts
+++ src/constants/errors.ts
@@ -4,5 +4,5 @@ export const metamaskErrors: Record<string, string> = {
   'insufficient funds for intrinsic transaction cost': 'Fondos insuficientes',
   'invalid decimal value': 'Valor decimal inválido',
   'Only the admin can withdraw the funds': 'Solo el Administrador del proyecto puede retirar los fondos',
-  'Project state not allowed': 'El estado del proyecto debe ser fondeado'
+  'Project state not allowed': 'El estado del proyecto debe ser inicializado o fondeado'
 }
